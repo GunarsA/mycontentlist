dbd3421b2d81cdd100fffbe67280a60212658ea3
Gunārs Ābeltiņš <gunars2003@gmail.com>
2023-06-23 19:22:11 +0300

    Redo seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,12 +4,15 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 use App\Models\Content;
 use App\Models\User;
 use App\Models\Staff;
 use App\Models\Role;
 use App\Models\Genre;
+use App\Models\Character;
 use App\Models\ContentGenre;
+
 
 class DatabaseSeeder extends Seeder
 {
@@ -25,76 +28,103 @@ class DatabaseSeeder extends Seeder
         //     'email' => 'test@example.com',
         // ]);
 
-        Content::create([
-            'title' => 'The Matrix',
-            'episode_cnt' => 1,
-            'length' => 136,
-            'year' => 1999,
-        ]);
-        Content::create([
-            'title' => 'The Matrix Reloaded',
-            'episode_cnt' => 1,
-            'length' => 138,
-            'year' => 2003,
-        ]);
-        Content::create([
-            'title' => 'The Matrix Revolutions',
-            'episode_cnt' => 1,
-            'length' => 129,
-            'year' => 2003,
-        ]);
-        Content::create([
-            'title' => 'The Office',
-            'episode_cnt' => 201,
-            'length' => 22,
-            'year' => 2005,
-        ]);
-
-        User::create([
+        DB::table('users')->insert([
             'name' => 'gunarsab',
             'email' => 'test@email.com',
             'password' => bcrypt('password'),
         ]);
 
-        Staff::create([
-            'name' => 'Keanu Reeves',
-        ]);
-        Staff::create([
-            'name' => 'Laurence Fishburne',
+        DB::table('contents')->insert([
+            [
+                'title' => 'The Matrix',
+                'episode_cnt' => 1,
+                'length' => 136,
+                'year' => 1999
+            ],
+            [
+                'title' => 'The Matrix Reloaded',
+                'episode_cnt' => 1,
+                'length' => 138,
+                'year' => 2003
+            ],
+            [
+                'title' => 'The Matrix Revolutions',
+                'episode_cnt' => 1,
+                'length' => 129,
+                'year' => 2003,
+            ],
+            [
+                'title' => 'The Office',
+                'episode_cnt' => 201,
+                'length' => 22,
+                'year' => 2005,
+            ],
         ]);
 
-        Role::create([
-            'content_id' => 1,
-            'staff_id' => 1,
-            'role' => 'Actor',
+        DB::table('staff')->insert([
+            [
+                'name' => 'Keanu Reeves',
+            ],
+            [
+                'name' => 'Laurence Fishburne',
+            ],
+            [
+                'name' => 'Lana Wachowski',
+            ],
+            [
+                'name' => 'Lilly Wachowski',
+            ],
         ]);
 
-        Role::create([
-            'content_id' => 1,
-            'staff_id' => 2,
-            'role' => 'Actor',
+        DB::table('genres')->insert([
+            [
+                'genre' => 'Action',
+            ],
+            [
+                'genre' => 'Sci-Fi',
+            ],
+            [
+                'genre' => 'Comedy',
+            ],
         ]);
 
-        Genre::create([
-            'genre' => 'Action',
+        DB::table('characters')->insert([
+            [
+                'name' => 'Neo',
+            ],
+            [
+                'name' => 'Morpheus',
+            ],
         ]);
 
-        Genre::create([
-            'genre' => 'Sci-Fi',
+        DB::table('roles')->insert([
+            [
+                'content_id' => 1,
+                'staff_id' => 1,
+                'role' => 'Actor',
+                'character_id' => 1,
+            ],
+            [
+                'content_id' => 1,
+                'staff_id' => 2,
+                'role' => 'Actor',
+                'character_id' => 2,
+            ],
+            [
+                'content_id' => 1,
+                'staff_id' => 3,
+                'role' => 'Director',
+                'character_id' => null,
+            ],
+            [
+                'content_id' => 1,
+                'staff_id' => 4,
+                'role' => 'Director',
+                'character_id' => null,
+            ],
         ]);
 
         $content = Content::find(1);
         $content->genres()->attach([1, 2]);
-
-        // ContentGenre::create([
-        //     'content_id' => 1,
-        //     'genre_id' => 1,
-        // ]);
-
-        // ContentGenre::create([
-        //     'content_id' => 1,
-        //     'genre_id' => 2,
-        // ]);
-
     }
 }
