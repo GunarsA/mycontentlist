f98734fd19b9d798dcc70005cbad04e4618ce8e0
ralfsa <greenapplera@gmail.com>
2023-06-25 12:49:33 +0300

    fixing layout

--- resources/views/content.blade.php
+++ resources/views/content.blade.php
@@ -1,55 +1,44 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Content Page</title>
-</head>
-
-<body>
-    <h1>Content Page</h1>
-    <a href={{ action([App\Http\Controllers\ContentController::class, 'create']) }}>New Content</a>
-    <table>
-        <thead>
-            <tr>
-                <th> Title </th>
-                <th> Type </th>
-                <th> Episode Count </th>
-                <th> Length </th>
-                <th> Year </th>
-                <th> Genres </th>
-                <th> Crew </th>
-                <th> Cast </th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach($content as $content)
-            <tr>
-                <td> <a href={{ route('content.show', ['content' => $content->id])}}>{{ $content->title }}</a> </td>
-                <td> {{ $content->type->type }} </td>
-                <td> {{ $content->episode_cnt }} </td>
-                <td> {{ $content->length }} </td>
-                <td> {{ $content->year }} </td>
-                <td>
-                    @foreach($content->genres as $genre)
-                    {{ $genre->genre }},
-                    @endforeach
-                <td>
-                <td>
-                    @foreach($content->crew as $crew)
-                    {{ $crew->name }} ({{ $crew->position }}),
-                    @endforeach
-                </td>
-                <td>
-                    @foreach($content->cast as $cast)
-                    {{ $cast->name }} ({{ $cast->character }}),
-                    @endforeach
-                </td>
-            </tr>
-            @endforeach
-        </tbody>
-    </table>
-</body>
-
-</html>
+@extends('layout')
+@section('content')
+<a href={{ action([App\Http\Controllers\ContentController::class, 'create']) }}>New Content</a>
+<table class="table-auto">
+    <thead>
+        <tr>
+            <th> Title </th>
+            <th> Type </th>
+            <th> Episode Count </th>
+            <th> Length </th>
+            <th> Year </th>
+            <th> Genres </th>
+            <th> Crew </th>
+            <th> Cast </th>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach($content as $content)
+        <tr>
+            <td> <a href={{ route('content.show', ['content' => $content->id])}}>{{ $content->title }}</a> </td>
+            <td> {{ $content->type->type }} </td>
+            <td> {{ $content->episode_cnt }} </td>
+            <td> {{ $content->length }} </td>
+            <td> {{ $content->year }} </td>
+            <td>
+                @foreach($content->genres as $genre)
+                {{ $genre->genre }},
+                @endforeach
+            <td>
+            <td>
+                @foreach($content->crew as $crew)
+                {{ $crew->name }} ({{ $crew->position }}),
+                @endforeach
+            </td>
+            <td>
+                @foreach($content->cast as $cast)
+                {{ $cast->name }} ({{ $cast->character }}),
+                @endforeach
+            </td>
+        </tr>
+        @endforeach
+    </tbody>
+</table>
+@endsection
--- resources/views/content_create.blade.php
+++ resources/views/content_create.blade.php
@@ -1,21 +1,72 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>New Content</title>
-</head>
-
-<body>
-    <form method="POST" action={{ action([App\Http\Controllers\ContentController::class, 'store']) }}>
+@extends('layout')
+@section('content')
+    <div class="grid place-items-center">
+        <form class="w-full max-w-lg">
+            <div class="flex flex-wrap -mx-3 mb-6">
+                <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+                    <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="title">
+                        Title
+                    </label>
+                    <input
+                        class="appearance-none block w-full bg-gray-50 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+                        id="title" type="text" placeholder="Batman Rises">
+                    <p class="text-red-500 text-xs italic">Please fill out this field.</p>
+                </div>
+                <div class="w-full md:w-1/2 px-3">
+                    <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="content_type">
+                        Content Type
+                    </label>
+                    <div class="relative">
+                        <select
+                            class="block appearance-none w-full bg-gray-50 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                            id="content_type">
+                            @foreach ($types as $type)
+                                <option value="{{ $type->id }}"
+                                    {{ old('content_type') == $type->id ? 'selected' : '' }}>
+                                    {{ $type->type }}</option>
+                            @endforeach
+                        </select>
+                        <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+                            <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                                <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
+                            </svg>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="flex flex-wrap -mx-3 mb-2">
+                <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
+                    <label for="episode_cnt"
+                        class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">Episode Count</label>
+                    <input type="text" name="episode_cnt" id="episode_cnt" required
+                        class="appearance-none block w-full bg-gray-50 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500">
+                </div>
+                <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
+                    <label for="length"
+                        class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">Length</label>
+                    <input type="text" name="length" id="length" required
+                        class="appearance-none block w-full bg-gray-50 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500">
+                </div>
+                <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
+                    <label for="year"
+                        class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">Year</label>
+                    <input type="text" name="year" id="year" required
+                        class="appearance-none block w-full bg-gray-50 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500">
+                </div>
+            </div>
+        </form>
+    </div>
+    <form method="POST" class="w-full max-w-lg"
+        action={{ action([App\Http\Controllers\ContentController::class, 'store']) }}>
         @csrf
         <label for="title">Title</label>
-        <input type="text" name="title" id="title" value="{{old('title')}}" required>
+        <input type="text" name="title" id="title" value="{{ old('title') }}" required>
         <label for="content_type">Content Type</label>
-        <select name="content_type" id="content_type">
-            @foreach($types as $type)
-            <option value="{{ $type->id }}" {{old('content_type') == $type->id ? "selected" : ""}}>{{ $type->type }}</option>
+        <select name="content_type" id="content_type"
+            class="h-full bg-gray-50 border border-gray-300 text-sm rounded focus:ring-blue-500 focus:border-blue-500 block w-24 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500">
+            @foreach ($types as $type)
+                <option value="{{ $type->id }}" {{ old('content_type') == $type->id ? 'selected' : '' }}>
+                    {{ $type->type }}</option>
             @endforeach
         </select>
         <label for="episode_cnt">Episode Count</label>
@@ -27,30 +78,28 @@
         <br>
 
         <label for="genre">Genre</label>
-        <select name="genre[]" id="genre" multiple>
-            @foreach($genres as $genre)
-            <option value="{{ $genre->id }}">{{ $genre->genre }}</option>
+        <select name="genre[]" id="genre" multiple class="">
+            @foreach ($genres as $genre)
+                <option value="{{ $genre->id }}">{{ $genre->genre }}</option>
             @endforeach
         </select>
         <label for="studio">Studio</label>
-        <select name="studio[]" id="studio" multiple>
-            @foreach($studios as $studio)
-            <option value="{{ $studio->id }}">{{ $studio->name }}</option>
+        <select name="studio[]" id="studio" multiple class="">
+            @foreach ($studios as $studio)
+                <option value="{{ $studio->id }}">{{ $studio->name }}</option>
             @endforeach
         </select>
 
-        @foreach($positions as $position)
-        <label for="{{ $position->position }}">{{ $position->position }}</label>
-        <select name="{{ $position->position }}[]" id="{{ $position->position }}" multiple>
-            @foreach($staff as $stafff)
-            <option value="{{ $stafff->id }}">{{ $stafff->name }}</option>
-            @endforeach
-        </select>
+        @foreach ($positions as $position)
+            <label for="{{ $position->position }}">{{ $position->position }}</label>
+            <select name="{{ $position->position }}[]" id="{{ $position->position }}" multiple>
+                @foreach ($staff as $stafff)
+                    <option value="{{ $stafff->id }}">{{ $stafff->name }}</option>
+                @endforeach
+            </select>
         @endforeach
 
-        <input type="submit" value="Submit">
+        <input type="submit" value="Submit"
+            class="bg-gray-50 border border-gray-300 text-sm rounded focus:ring-blue-500 focus:border-blue-500 w-20 p-1 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500">
     </form>
-
-</body>
-
-</html>
+@endsection
