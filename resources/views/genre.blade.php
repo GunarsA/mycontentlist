f776650efee0b59b2833e6e71dd66423edd5ecda
ralfsa <greenapplera@gmail.com>
2023-06-25 15:49:17 +0300

    changed genre, genre edit, layout

--- resources/views/genre.blade.php
+++ resources/views/genre.blade.php
@@ -1,28 +1,20 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Genres</title>
-</head>
-
-<body>
-    <h1>Genres</h1>
-    <a href={{ action([App\Http\Controllers\GenreController::class, 'create']) }}>New Genre</a>
-    <ul>
+@extends('layout')
+@section('content')
+<div class="container mx-auto">
+    <h1 class="text-2xl font-bold mb-4">Genres</h1>
+    <a href="{{ action([App\Http\Controllers\GenreController::class, 'create']) }}" class="inline-block px-4 py-2 mb-4 bg-blue-500 text-white rounded hover:bg-blue-600">New Genre</a>
+    <ul class="space-y-2">
         @foreach ($genres as $genre)
-        <li>
-            <a href="{{ route('genre.show', ['genre' => $genre->id])}}">{{ $genre->genre }}</a>
-            <form method="POST" action="{{ route('genre.destroy', $genre->id) }}">
+        <li class="flex items-center justify-between bg-white rounded-lg p-4 shadow-md">
+            <a href="{{ route('genre.show', ['genre' => $genre->id]) }}" class="text-blue-500 hover:underline">{{ $genre->genre }}</a>
+            <form method="POST" action="{{ route('genre.destroy', $genre->id) }}" class="inline-block">
                 @csrf
                 @method('DELETE')
-
-                <button type="submit" value="delete">Delete</button>
+                <button type="submit" value="delete" class="text-red-500 hover:text-red-700">Delete</button>
             </form>
         </li>
         @endforeach
     </ul>
-</body>
+</div>
 
-</html>
+@endsection
--- resources/views/genre_edit.blade.php
+++ resources/views/genre_edit.blade.php
@@ -4,15 +4,15 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Edit Staff</title>
+    <title>Edit Genre</title>
 </head>
 
 <body>
-    <h1>Edit Staff</h1>
+    <h1>Edit Genre</h1>
     <form method="POST" action={{ action([App\Http\Controllers\GenreController::class, 'update'], ['genre' => $genre]) }}>
         @csrf
         @method('PUT')
-        
+
         <label for="genre">Genre</label>
         <input type="text" name="genre" id="genre" value="{{old('genre', $genre->genre)}}" required>
     </form>
