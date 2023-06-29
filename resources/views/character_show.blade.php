4c32aa810ce58387610659fe280851f7c84a4d5c
ralfsa <greenapplera@gmail.com>
2023-06-29 09:06:32 +0300

    yeah

--- resources/views/character_show.blade.php
+++ resources/views/character_show.blade.php
@@ -1,12 +1,13 @@
 @extends('layout')
 @section('content')
-    <h1>{{ $character->name }}</h1>
-    <a href={{ action([App\Http\Controllers\CharacterController::class, 'edit'], ['character' => $character]) }}>Edit</a>
+<h1 class="text-3xl font-bold text-black">{{ $character->name }}</h1>
+<a href={{ action([App\Http\Controllers\CharacterController::class, 'edit'], ['character' => $character]) }} class="text-blue-500 hover:underline">Edit</a>
 
-    <h3>Content</h3>
-    <ul>
-        @foreach ($character->content as $content)
-        <li><a href={{ action([App\Http\Controllers\ContentController::class, 'show'], ['content' => $content]) }}>{{ $content->title }}</a></li>
-        @endforeach
-    </ul>
+<h3 class="text-lg font-semibold text-black mt-6">Content</h3>
+<ul class="mt-4">
+    @foreach ($character->content as $content)
+    <li><a href={{ action([App\Http\Controllers\ContentController::class, 'show'], ['content' => $content]) }} class="text-gray-700 hover:text-gray-900">{{ $content->title }}</a></li>
+    @endforeach
+</ul>
+
 @endsection
--- resources/views/content_show.blade.php
+++ resources/views/content_show.blade.php
@@ -1,46 +1,47 @@
 @extends('layout')
 @section('content')
-    <h1>{{$content->title}}</h1>
-    @if (Auth::check() && Auth::user()->can('modify'))
-    <a href={{ action([App\Http\Controllers\ContentController::class, 'edit'], ['content' => $content]) }}>Edit</a>
-    @endif
-    @if (Auth::check() && Auth::user()->can('modify'))
-    <form action={{ action([App\Http\Controllers\ContentController::class, 'destroy'], ['content' => $content]) }} method="POST">
-        @csrf
-        @method('DELETE')
-        <button type="submit">Delete</button>
-    </form>
-    @endif
-    <h3>Content Type</h3>
-    <p>{{ $content->type->type }}</p>
-    <h3>Episode Count</h3>
-    <p>{{ $content->episode_cnt }}</p>
-    <h3>Length</h3>
-    <p>{{ $content->length }}</p>
-    <h3>Year</h3>
-    <p>{{ $content->year }}</p>
-    <h3>Genres</h3>
-    <ul>
-        @foreach($content->genres as $genre)
-        <li>{{ $genre->genre }}</li>
-        @endforeach
-    </ul>
-    <h3>Studios</h3>
-    <ul>
-        @foreach($content->studios as $studio)
-        <li>{{ $studio->name }}</li>
-        @endforeach
-    </ul>
-    <h3>Crew & Cast</h3>
-    <ul>
-        @foreach($content->staff as $staff)
-        <li>{{ $staff->name }} ({{ $staff->position }})</li>
-        @endforeach
-    </ul>
-    <h3>Characters</h3>
-    <ul>
-        @foreach($content->characters as $character)
-        <li>{{ $character->name }}</li>
-        @endforeach
-    </ul>
+<h1 class="text-3xl font-bold text-black">{{$content->title}}</h1>
+@if (Auth::check() && Auth::user()->can('modify'))
+<a href={{ action([App\Http\Controllers\ContentController::class, 'edit'], ['content' => $content]) }} class="text-blue-500 hover:underline">{{ __('gen.edit') }}</a>
+@endif
+@if (Auth::check() && Auth::user()->can('modify'))
+<form action={{ action([App\Http\Controllers\ContentController::class, 'destroy'], ['content' => $content]) }} method="POST" class="inline-block">
+    @csrf
+    @method('DELETE')
+    <button type="submit" class="text-red-500 hover:text-red-700">{{ __('gen.delete') }}</button>
+</form>
+@endif
+<h3 class="text-lg font-semibold text-black mt-6">{{ __('content.type') }}</h3>
+<p class="mt-2">{{$content->type->type}}</p>
+<h3 class="text-lg font-semibold text-black mt-6">{{ __('content.ep_cnt') }}</h3>
+<p class="mt-2">{{$content->episode_cnt}}</p>
+<h3 class="text-lg font-semibold text-black mt-6">{{ __('content.length') }}</h3>
+<p class="mt-2">{{$content->length}}</p>
+<h3 class="text-lg font-semibold text-black mt-6">{{ __('content.year') }}</h3>
+<p class="mt-2">{{$content->year}}</p>
+<h3 class="text-lg font-semibold text-black mt-6">{{ __('content.genres') }}</h3>
+<ul class="mt-2">
+    @foreach($content->genres as $genre)
+    <li>{{$genre->genre}}</li>
+    @endforeach
+</ul>
+<h3 class="text-lg font-semibold text-black mt-6">{{ __('gen.studios') }}</h3>
+<ul class="mt-2">
+    @foreach($content->studios as $studio)
+    <li>{{$studio->name}}</li>
+    @endforeach
+</ul>
+<h3 class="text-lg font-semibold text-black mt-6">Cast</h3>
+<ul class="mt-2">
+    @foreach($content->staff as $staff)
+    <li>{{$staff->name}} ({{$staff->position}})</li>
+    @endforeach
+</ul>
+<h3 class="text-lg font-semibold text-black mt-6">{{ __('content.characters') }}</h3>
+<ul class="mt-2">
+    @foreach($content->characters as $character)
+    <li>{{$character->name}}</li>
+    @endforeach
+</ul>
+
 @endsection
